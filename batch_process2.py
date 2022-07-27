06e45270eb844ee447a2158964af855abffdfc7a
SequelSaturn <108498033+SequelSaturn@users.noreply.github.com>
2022-07-26 23:11:59 -0400

    Add files via upload

---
+++ batch_process2.py
@@ -0,0 +1,6 @@
+import os
+import time
+
+for jj in range(1,105):
+    str0 = "python generate.py.py --outdir=out/projector" + str(jj) + " --dlatents=out/projector" + str(jj) + "/dlatents.npz --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada/pretrained/ffhq.pkl"
+    os.system(str1)
