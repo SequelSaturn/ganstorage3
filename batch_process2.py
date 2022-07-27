5c5d24404229b8b88ec420e8e948d82e3f626e45
SequelSaturn <108498033+SequelSaturn@users.noreply.github.com>
2022-07-27 18:35:20 -0400

    Update batch_process2.py

--- batch_process2.py
+++ batch_process2.py
@@ -3,4 +3,4 @@ import time
 
 for jj in range(1,105):
     str0 = "python generate.py.py --outdir=out/projector-latents" + str(jj) + " --dlatents=out/projector" + str(jj) + "/dlatents.npz --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada/pretrained/ffhq.pkl"
-    os.system(str1)
+    os.system(str0)
